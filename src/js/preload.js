bad808911e210b62b615d70720774c9c7f730c96
Edson Rocha <rocha.edsonn@gmail.com>
2024-12-10 16:07:50 -0300

    correção do js  do preload

--- src/js/preload.js
+++ src/js/preload.js
@@ -1,5 +1,11 @@
 window.addEventListener('load', () => {
-    const preload = document.getElementById('preload');
-    preload.style.opacity = '0';
-    setTimeout(() => preload.style.display = 'none', 500); // Esconde após a transição
+    const bgPreload = document.querySelector('.bg-preload');
+    
+    // Adiciona a classe para ocultar o preloader com transição
+    bgPreload.classList.add('hidden');
+    
+    // Remove completamente o elemento após a transição
+    setTimeout(() => {
+        bgPreload.remove();
+    }, 500); // Tempo correspondente ao da transição no CSS
 });
